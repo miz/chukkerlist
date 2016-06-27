e7010da2fa745faccfd23011d1b463937abe7469
miz <michaelmizrahi@me.com>
2016-06-26 19:38:08 -0700

    Rudimentary version of the app working.

--- methods.rb
+++ methods.rb
@@ -1,10 +1,27 @@
-# Print out that we're adding the chukkers of the two players together
-# And then return the sum of those two players
-def chukker_count(player_a,player_b)
-	puts "Adding #{player_a} and #{player_b}"
-	return player_a + player_b
-end
+# Get player a's name and set it to var player_b
+print "Who is player 1? "
+player_a = gets
 
-puts chukker_count(4,3)
-chukker_count(5,5)
-chukker_count(4,4)
+# Ask player a how many chukkers
+print "How many chukkers for #{player_a}?"
+# Convert the string to an integer with .to_i
+count1 = gets.to_i
+
+# Get player b's name and set it to var player_b
+print "Who is player 2? "
+player_b = gets
+
+# Ask player b how many chukkers
+print "How many chukkers for #{player_b}?\n"
+count2 = gets.to_i
+
+# Time for results. Sum the count of chukkers:
+total_count = count1 + count2
+# Divide the sum by 4 (for the number of players)
+chukker_count = total_count / 4
+# And provide the remainder of that division (if any) to solve for the number of spots open in the last period.
+chukker_modulus = total_count % 8
+# Print results
+print "There are a total of #{total_count} player-chukkers.\n
+This means there will be #{chukker_count} chukkers.\n
+There are #{chukker_modulus} open spots to fill the last chukker."
